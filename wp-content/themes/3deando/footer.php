8fa66d5116281f60d149fad97b057216a48d7e97
Mariano Timistit <mariano@wcanvas.com>
2020-11-21 11:15:33 -0300

    Setup

---
+++ wp-content/themes/3deando/footer.php
@@ -0,0 +1,21 @@
+    <footer>
+        <div class="container container-xl">
+            <!-- Contact Info -->
+            <div class="contact-info">
+                Contact Info
+            </div>
+            <!-- Social -->
+            <div class="social">
+                Social
+            </div>
+            <!-- Legal -->
+            <div class="legal">
+                <h6>Copyright © 2020 Creando 3D - Powered by Creando 3D</h6>
+            </div>
+        </div>
+    </footer>
+
+    <?php wp_footer(); ?>
+
+    </body>
+</html>
